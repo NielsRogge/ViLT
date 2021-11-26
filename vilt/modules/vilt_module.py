3de8294d4de9482a96b3c3fe133263df5cc0037f
Niels Rogge <niels.rogge1@gmail.com>
2021-11-26 11:37:35 +0100

    Add print statements

--- vilt/modules/vilt_module.py
+++ vilt/modules/vilt_module.py
@@ -164,7 +164,6 @@ class ViLTransformerSS(pl.LightningModule):
         for i, blk in enumerate(self.transformer.blocks):
             print(f"Hidden states before layer {i}", x[0,:3,:3])
             x, _attn = blk(x, mask=co_masks)
-            print(f"Hidden states after layer {i}", x[0,:3,:3])
 
         x = self.transformer.norm(x)
         text_feats, image_feats = (
